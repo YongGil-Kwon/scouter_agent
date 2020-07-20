d3ecaa68448b2ea24ce552c04794f5365ea80dee
trois <trois@neowiz.com>
2020-07-20 20:49:37 +0900

    entrypoint.sh 추가

--- agent.host/entrypoint.sh
+++ agent.host/entrypoint.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-java  -classpath ./scouter.host.jar scouter.boot.Boot ./lib
+nohup java -classpath /neowiz/scouter_agent/agent.host/scouter.host.jar scouter.boot.Boot /neowiz/scouter_agent/agent.host/lib > /neowiz/scouter_agent/agent.host/nohup.out &
+sleep 3
+nohup java $JAVA_OPTS -jar /neowiz/neon-push.war &
